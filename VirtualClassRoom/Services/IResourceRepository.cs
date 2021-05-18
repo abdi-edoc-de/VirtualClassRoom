0811c4386529f9fbae32af98a23b2c680e7c5cc8
Abdi <se.abdulfeta.dedgba@gmail.com>
2021-05-18 16:49:41 +0300

    Added ResourceRepository

---
+++ VirtualClassRoom/Services/IResourceRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualClassRoom.Entities;
+
+namespace VirtualClassRoom.Services
+{
+    public interface IResourceRepository
+    {
+        public void AddResources(Resource resource);
+        public void DeleteResource(Guid resourceId);
+        public Resource GetResource(Guid resourceId);
+        public IEnumerable<Resource> GetResources(Guid courseId);
+
+    }
+}
---
+++ VirtualClassRoom/Services/ResourceRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualClassRoom.DbContexts;
+using VirtualClassRoom.Entities;
+
+namespace VirtualClassRoom.Services
+{
+    public class ResourceRepository : IResourceRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ResourceRepository(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext ?? throw new ArgumentNullException(nameof(appDbContext));
+        }
+        public void AddResources(Resource resource)
+        {
+            if (resource == null)
+            {
+                throw new ArgumentNullException(nameof(resource));
+            }
+            _appDbContext.Resources.Add(resource);
+            _appDbContext.SaveChanges();
+        }
+
+        public void DeleteResource(Guid resourceId)
+        {
+            if( resourceId == Guid.Empty)
+            {
+                throw new ArgumentNullException(nameof(resourceId));
+            }
+            Resource resource = _appDbContext.Resources.FirstOrDefault(r => r.ResourceId == resourceId) ??
+                throw new ArgumentNullException(nameof(resource));
+            _appDbContext.Resources.Remove(resource);
+        }
+
+        public Resource GetResource(Guid resourceId)
+        {
+            if(resourceId == Guid.Empty)
+            {
+                throw new ArgumentNullException(nameof(resourceId));
+
+            }
+            Resource resource = _appDbContext.Resources.FirstOrDefault(r => r.ResourceId == resourceId) ??
+                throw new ArgumentNullException(nameof(resource));
+            return resource;
+        }
+
+        public IEnumerable<Resource> GetResources(Guid courseId)
+        {
+            if (courseId == Guid.Empty)
+            {
+                throw new ArgumentNullException(nameof(courseId));
+
+            }
+            IEnumerable<Resource> resources = _appDbContext.Resources.Where(r => r.CourseId == courseId).ToList() ??
+                throw new ArgumentNullException(nameof(resources));
+            return resources;
+        }
+    }
+}
