1bff9e4f0b41644604fb05eb5bce22bbdeaa082e
Abdi <se.abdulfeta.dedgba@gmail.com>
2021-05-30 16:17:55 +0300

    d

--- VirtualClassRoom/Controllers/StudentController.cs
+++ VirtualClassRoom/Controllers/StudentController.cs
@@ -125,7 +125,7 @@ namespace VirtualClassRoom.Controllers
             return Ok(studentToReturn);
 
         }
-        [HttpGet("{studentEmail}")]
+        [HttpGet("StudentEmail/{studentEmail}")]
         public async Task<ActionResult<UserDto>> GetOneStudentInfoByEmail(string studentEmail)
         {
 
